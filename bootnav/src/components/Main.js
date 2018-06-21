17d83094f2849cac94474632c1394687347e8ab4
Justen Falk <>
2018-06-21 11:08:59 -0400

    Map component

--- bootnav/src/components/Main.js
+++ bootnav/src/components/Main.js
@@ -1,5 +1,21 @@
 import React from "react";
-import { GoogleMap, Marker } from "react-google-maps";
+import { compose, withProps } from "recompose"
+import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
+
+const MyMapComponent = compose(
+  withProps({
+    googleMapURL: "https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places",
+    loadingElement: <div style={{ height: `100%` }} />,
+    containerElement: <div style={{ height: `400px` }} />,
+    mapElement: <div style={{ height: `100%` }} />,
+  }),
+  withScriptjs,
+  withGoogleMap
+)(props =>
+  <GoogleMap defaultZoom={8} defaultCenter={{ lat: -34.397, lng: 150.644 }}>
+    {props.markers}
+  </GoogleMap>
+)
 
 export default class Main extends React.Component {
   constructor(props) {
@@ -9,24 +25,27 @@ export default class Main extends React.Component {
       zoneCenterPoint: {},
       defaultZoomLevel: 8
     };
+    this.handleMarkerClick = this.handleMarkerClick.bind(this);
   }
 
-  _getHits() {
-    //fetch hits
+  componentDidMount() {
+    let self = this;
+    fetch('/hits').then(res => self.state.hits = res);
+  }
+
+  handleMarkerClick(marker) {
+    alert(JSON.stringify(marker));
   }
 
   render() {
+    const markers = this.state.hits.map((marker, idx) => (
+      <Marker key={idx} position={{ lat: marker.Latitude, lng: marker.Longitude }} onClick={e => this.handleMarkerClick(marker)}> </Marker>
+    ));
+
     return (
-      <div>
-        <GoogleMap
-          defaultZoom={8}
-          defaultCenter={{ lat: -34.397, lng: 150.644 }}
-        >
-          {props.isMarkerShown && (
-            <Marker position={{ lat: -34.397, lng: 150.644 }} />
-          )}
-        </GoogleMap>
-      </div>
+      <MyMapComponent
+        markers={this.state.hits}
+      />
     );
   }
 }
